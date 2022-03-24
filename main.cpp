6aa7fe11dc1f6c95f87f56158288caaba3291e13
MedonisMayhem <88583896+MedonisMayhem@users.noreply.github.com>
2022-03-24 12:52:38 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+auto recursiveGetFileNamesByExtension =
+[](fs::path path, const std::string extension)
+{
+    for (auto& p : fs::recursive_directory_iterator(path))
+    {
+        if (p.is_regular_file() && !p.path().extension().compare("." + extension))
+        {
+            std::cout << p.path().stem() << "\n";
+        }
+    }
+};
+auto main() -> int {
+    fs::path p("D:\\images");
+    
+    recursiveGetFileNamesByExtension(p, "jpg");
+
+
+
+    //p.is_regular_file() && 
+}
